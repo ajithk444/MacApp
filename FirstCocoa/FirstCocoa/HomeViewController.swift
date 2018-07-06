b56023ea77b365fdc88a60b65c4a6bf37c1ec611
Ajith <ajithk444@gmail.com>
2018-07-06 21:49:43 +0530

    commit

---
+++ FirstCocoa/FirstCocoa/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  HomeViewController.swift
+//  FirstCocoa
+//
+//  Created by Ajith Kumar on 06/07/18.
+//  Copyright © 2018 Ajith Kumar. All rights reserved.
+//
+
+import Cocoa
+
+class HomeViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
